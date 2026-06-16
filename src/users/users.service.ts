0fa5da2e56c7bf6370706487240186fcf9db67c6
Ngoc Minh Tran <mingtran@iu.edu>
2026-06-16 15:41:03 +0700

    CRU users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entitiy';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, username, password } = createUserDto;
+
+    const existingUser = await this.usersRepository.findOne({
+      where: [{ email }, { username }],
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Email or username already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = this.usersRepository.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    const savedUser = await this.usersRepository.save(user);
+
+    const { password: _, ...result } = savedUser;
+    return result;
+  }
+}
