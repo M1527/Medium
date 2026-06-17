ed95728950f3daf5fe290414b8e4355c286c601d
Ngoc Minh Tran <mingtran@iu.edu>
2026-06-17 17:03:08 +0700

    refactor: move jwt secret to environment variables

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule,
-    JwtModule.register({
-      secret: 'medium_clone_secret',
-    }),
+    JwtModule.registerAsync({
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    secret: configService.get<string>('JWT_SECRET'),
+    signOptions: {
+      expiresIn: '1d',
+    },
+  }),
+})
   ],
   providers: [JwtStrategy],
   exports: [JwtModule, PassportModule],
